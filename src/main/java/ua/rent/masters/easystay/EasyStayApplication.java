c9d2cca1520bdd418c5c9bc855aefb0e68904fd4
Artem Akymenko <akimenko.artem@gmail.com>
2024-05-29 17:48:32 +0300

    Initial commit

---
+++ src/main/java/ua/rent/masters/easystay/EasyStayApplication.java
@@ -0,0 +1,13 @@
+package ua.rent.masters.easystay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyStayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyStayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/rent/masters/easystay/EasyStayApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.rent.masters.easystay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
