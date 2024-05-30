c8fb51bd915d02d7d6b606b35c27a61942e09631
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-05-30 15:09:05 +0300

    added role to liqubase and constrains for user

--- src/main/java/ua/rent/masters/easystay/model/User.java
+++ src/main/java/ua/rent/masters/easystay/model/User.java
@@ -1,5 +1,6 @@
 package ua.rent.masters.easystay.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -7,12 +8,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
 @Getter
 @Setter
 @EqualsAndHashCode
@@ -22,10 +28,15 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 
