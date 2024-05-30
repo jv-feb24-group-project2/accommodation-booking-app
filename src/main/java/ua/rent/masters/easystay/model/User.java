e46329fd4aaecc95e8b4d5b00255d0860806ef9f
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-05-30 15:19:22 +0300

    fixed checkstyle

--- src/main/java/ua/rent/masters/easystay/model/User.java
+++ src/main/java/ua/rent/masters/easystay/model/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
