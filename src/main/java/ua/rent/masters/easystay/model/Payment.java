26895aa043c0ec8f2ce4a010d7c9325fd814051b
Taras Fedak <tarasfedak@me.com>
2024-05-31 14:58:15 +0300

    Fixed

--- src/main/java/ua/rent/masters/easystay/model/Payment.java
+++ src/main/java/ua/rent/masters/easystay/model/Payment.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
