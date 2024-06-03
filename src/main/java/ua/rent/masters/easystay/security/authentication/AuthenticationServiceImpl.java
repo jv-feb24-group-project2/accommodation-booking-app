7e863bcc00800d71ba16397dc1bcb9eceaae44c1
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-06-03 19:11:28 +0300

    Added added Security Layer & Role Layer

---
+++ src/main/java/ua/rent/masters/easystay/security/authentication/AuthenticationServiceImpl.java
@@ -0,0 +1,27 @@
+package ua.rent.masters.easystay.security.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import ua.rent.masters.easystay.dto.user.login.UserLoginRequestDto;
+import ua.rent.masters.easystay.dto.user.login.UserLoginResponseDto;
+import ua.rent.masters.easystay.security.JwtUtil;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public UserLoginResponseDto authenticate(UserLoginRequestDto userLoginRequestDto) {
+        final Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userLoginRequestDto.email(),
+                        userLoginRequestDto.password())
+        );
+        String token = jwtUtil.generateToken(authentication.getName());
+        return new UserLoginResponseDto(token);
+    }
+}
