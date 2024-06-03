65435968b0d2edf0dca65d016a4ec608f42bca8f
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-06-04 01:34:17 +0300

    Added payments acces to config

--- src/main/java/ua/rent/masters/easystay/config/SecurityConfig.java
+++ src/main/java/ua/rent/masters/easystay/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         auth -> auth
                                 .requestMatchers(
+                                        "/payments/**",
                                         "/auth/**",
                                         "/error",
                                         "/swagger-ui/**",
