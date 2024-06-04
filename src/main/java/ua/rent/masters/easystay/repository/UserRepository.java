52f3656045d995c7daa738038340328cbd7aa7db
Artem Akymenko <113580621+k0sm0naft@users.noreply.github.com>
2024-06-05 00:29:06 +0300

    Adding Security To Notification (#25)
    Minor fixes

--- src/main/java/ua/rent/masters/easystay/repository/UserRepository.java
+++ src/main/java/ua/rent/masters/easystay/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package ua.rent.masters.easystay.repository;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ua.rent.masters.easystay.model.Role;
 import ua.rent.masters.easystay.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +13,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
     Optional<User> getByChatId(long chatId);
+
+    @Query("SELECT u FROM User u JOIN u.roles r WHERE r.name = :roleName")
+    List<User> findByRoleName(Role.RoleName roleName);
+
+    Optional<User> getByIdAndEmail(Long id, String email);
 }
