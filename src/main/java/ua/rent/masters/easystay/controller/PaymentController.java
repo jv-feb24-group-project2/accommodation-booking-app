676ae73edac39bf1f8a8c9b036a08a17c09f078c
Taras Fedak <tarasfedak@me.com>
2024-06-04 15:55:08 +0300

    Added Swagger initial commit

--- src/main/java/ua/rent/masters/easystay/controller/PaymentController.java
+++ src/main/java/ua/rent/masters/easystay/controller/PaymentController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,7 +51,7 @@ public class PaymentController {
     @ResponseStatus(HttpStatus.SEE_OTHER)
     @GetMapping("/create-session/{bookingId}")
     public void createPaymentSession(@PathVariable Long bookingId,
-                                                     HttpServletResponse response) throws Exception {
+                                     HttpServletResponse response) throws Exception {
         String sessionUrl = paymentService.createPaymentSession(bookingId);
         response.setHeader(HttpHeaders.LOCATION, sessionUrl);
     }
