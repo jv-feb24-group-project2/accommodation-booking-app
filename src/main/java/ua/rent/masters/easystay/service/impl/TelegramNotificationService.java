996e458edccfd02a1adf0e4089dfd4cf23c21923
Artem Akymenko <113580621+k0sm0naft@users.noreply.github.com>
2024-06-05 02:28:22 +0300

    Fixed notification of all managers (#30)

--- src/main/java/ua/rent/masters/easystay/service/impl/TelegramNotificationService.java
+++ src/main/java/ua/rent/masters/easystay/service/impl/TelegramNotificationService.java
@@ -39,17 +39,14 @@ public class TelegramNotificationService implements NotificationService {
             Accommodation accommodation, AccommodationStatus status
     ) {
         String message = switch (status) {
-            case CREATED ->
-                    "New accommodation listed: %s in %s, $%s/day."
-                            .formatted(accommodation.getType(), accommodation.getLocation(),
-                                    accommodation.getDailyRate());
-            case UPDATED ->
-                    "Accommodation updated: %s in %s, new rate $%s/day."
-                            .formatted(accommodation.getType(), accommodation.getLocation(),
-                                    accommodation.getDailyRate());
-            case DELETED ->
-                    "Accommodation removed: %s in %s."
-                            .formatted(accommodation.getType(), accommodation.getLocation());
+            case CREATED -> "New accommodation listed: %s in %s, $%s/day."
+                    .formatted(accommodation.getType(), accommodation.getLocation(),
+                            accommodation.getDailyRate());
+            case UPDATED -> "Accommodation updated: %s in %s, new rate $%s/day."
+                    .formatted(accommodation.getType(), accommodation.getLocation(),
+                            accommodation.getDailyRate());
+            case DELETED -> "Accommodation removed: %s in %s."
+                    .formatted(accommodation.getType(), accommodation.getLocation());
         };
         sendToAllManagers(message);
     }
@@ -60,21 +57,17 @@ public class TelegramNotificationService implements NotificationService {
             return;
         }
         String message = switch (status) {
-            case PENDING ->
-                    "Your booking for Accommodation ID%s from %s to %s is pending."
-                            .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
+            case PENDING -> "Your booking for Accommodation ID%s from %s to %s is pending."
+                    .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
                             booking.getCheckOutDate());
-            case CONFIRMED ->
-                    "Your booking for Accommodation ID%s from %s to %s is confirmed."
-                            .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
+            case CONFIRMED -> "Your booking for Accommodation ID%s from %s to %s is confirmed."
+                    .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
                             booking.getCheckOutDate());
-            case CANCELED ->
-                    "Your booking for Accommodation ID%s from %s to %s has been canceled."
-                            .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
+            case CANCELED -> "Your booking for Accommodation ID%s from %s to %s has been canceled."
+                    .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
                             booking.getCheckOutDate());
-            case EXPIRED ->
-                    "Your booking for Accommodation ID%s from %s to %s has expired."
-                            .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
+            case EXPIRED -> "Your booking for Accommodation ID%s from %s to %s has expired."
+                    .formatted(booking.getAccommodationId(), booking.getCheckInDate(),
                             booking.getCheckOutDate());
         };
         botHandler.send(user.getChatId(), message);
@@ -155,7 +148,8 @@ public class TelegramNotificationService implements NotificationService {
     }
 
     private void sendToAllManagers(String message) {
-        userRepository.findByRoleName(Role.RoleName.ROLE_MANAGER)
-                      .forEach(u -> botHandler.send(u.getChatId(), message));
+        userRepository.findByRoleName(Role.RoleName.ROLE_MANAGER).stream()
+                      .filter(user -> user.getChatId() != null)
+                      .forEach(user -> botHandler.send(user.getChatId(), message));
     }
 }
