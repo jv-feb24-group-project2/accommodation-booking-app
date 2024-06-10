96970e365022f2d20013fde3d03e3921a51174f7
Artem Akymenko <akimenko.artem@gmail.com>
2024-06-10 18:26:50 +0300

    Added integration tests for user controller

---
+++ src/test/resources/database/tear-down-db.sql
@@ -0,0 +1,8 @@
+DELETE FROM payments;
+DELETE FROM bookings;
+DELETE FROM accommodations_amenities;
+DELETE FROM amenities;
+DELETE FROM accommodations;
+DELETE FROM users_roles;
+DELETE FROM roles;
+DELETE FROM users;
