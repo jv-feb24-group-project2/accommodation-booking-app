55208109ea7271d2e77dbf5ad7ba220b6005fab5
Hanna Ratushniak <ratushnyakhanna@gmail.com>
2024-06-12 01:37:28 +0300

    Added changes according the review

--- src/main/java/ua/rent/masters/easystay/controller/AccommodationController.java
+++ src/main/java/ua/rent/masters/easystay/controller/AccommodationController.java
@@ -3,8 +3,9 @@ package ua.rent.masters.easystay.controller;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ua.rent.masters.easystay.dto.accommodation.AccommodationRequestDto;
@@ -29,9 +29,10 @@ public class AccommodationController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<AccommodationResponseDto> getAll(@RequestParam(defaultValue = "0") int page,
-                                                 @RequestParam(defaultValue = "10") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+    public List<AccommodationResponseDto> getAll(
+            @ParameterObject
+            @PageableDefault(sort = {"id", "type"}, value = 10) Pageable pageable
+    ) {
         return accommodationService.findAll(pageable);
     }
 
